3e3c1f99b6e19236ff1f8899865e4d1fcaf00f29
emranHKhan <emran.khan2400@gmail.com>
2024-07-15 04:14:05 +0600

    show all url list

--- online_school_backend/urls.py
+++ online_school_backend/urls.py
@@ -1,5 +1,46 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.http import HttpResponse
+from django.urls import get_resolver, path, include, reverse
+from django.utils.html import format_html
+
+url_descriptions = {
+    'user-detail': 'Detail view of a specific user',
+    'department-list': 'List all departments',
+    'course-list': 'List all courses (by teacher and department)',
+    'course-detail': 'Detail view of a specific course',
+    'enrollment-list': 'List all enrollments',
+    'enrollments-by-student': 'List all enrollments for a specific student',
+    'comment-list': 'List all comments',
+    'register': 'User registration',
+    'login': 'User login',
+    'logout': 'User logout',
+    'course-create': 'Create a new course',
+    'teacher-list': 'List all teachers',
+    'user-list': 'List all users'
+}
+
+def list_urls(request):
+    urlconf = get_resolver()
+    urls = urlconf.reverse_dict.keys()
+    scheme = request.scheme
+    host = request.get_host()
+    base_url = f"{scheme}://{host}/"
+    
+    response = '<h1>Available URLs</h1><ul>'
+    for url in urls:
+        if url == 'list_urls' or url == 'activate':
+            continue
+        if isinstance(url, str):  
+            try:
+                url_path = reverse(url)
+                full_url = base_url + url_path.lstrip('/')
+                description = url_descriptions.get(url, 'No description available')
+                response += format_html('<li><a href="{}">{}</a> - {}</li>', full_url, url, description)
+            except Exception as e:
+                response += format_html('<li>{} (URL requires parameters) - {}</li>', url, description)
+    response += '</ul>'
+    return HttpResponse(response)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +49,5 @@ urlpatterns = [
     path('api/', include('enrollments.urls')),
     path('api/', include('comments.urls')),
     path('api/', include('courses.urls')),
+    path('', list_urls, name='list_urls'),
 ]
